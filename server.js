a0f3c0cdc5d5b267e32e04b5e1c097e96018e682
Bilal Minhas <bilal.minhas@c02d32htjv3x.wren.wrenkitchens.com>
2020-08-24 15:09:45 +0100

    set up a template engine

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+app.set('view engine', 'pug');
+
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+  res.render(`pug`);
 });
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
